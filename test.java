cc8f8644717b79635da40e9eb55b3c4edb342aad
Bui Anh Quoc <anhquocydkl@gmail.com>
2024-08-20 19:01:11 +0700

    Add

--- test.java
+++ test.java
@@ -1,42 +1,41 @@
-import java.util.Scanner;
 
 public class test 
     {
 
         public static void main(String[] args) 
          {
-            int a[][];
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Nhap so hang: ");
-            int n = scanner.nextInt();
-            System.out.print("Nhap so cot: ");
-            int m = scanner.nextInt();
+            // int a[][];
+            // Scanner scanner = new Scanner(System.in);
+            // System.out.print("Nhap so hang: ");
+            // int n = scanner.nextInt();
+            // System.out.print("Nhap so cot: ");
+            // int m = scanner.nextInt();
 
-            a = new int [n][m];
+            // a = new int [n][m];
 
-            for (int i = 0 ; i < n ; i++)
-            {
-                System.out.println();
-                for ( int j = 0 ; j < m ; j++)
-                {
-                    System.out.print("Nhap gia tri a["+(i+1)+"]" + "["+(j+1)+"]: ");
-                    a[i][j] = scanner.nextInt();
-                }
-            }
+            // for (int i = 0 ; i < n ; i++)
+            // {
+            //     System.out.println();
+            //     for ( int j = 0 ; j < m ; j++)
+            //     {
+            //         System.out.print("Nhap gia tri a["+(i+1)+"]" + "["+(j+1)+"]: ");
+            //         a[i][j] = scanner.nextInt();
+            //     }
+            // }
 
-            System.out.println();
-            System.out.println("Ma tran vua nhap voi " + n + " hang va " + m + " cot:");
-            for (int i = 0 ; i < n ; i++)
-            {
-                System.out.println();
-                for(int j = 0 ; j < m ; j++)
-                {
-                    System.out.print(a[i][j] + " ");
-                }
-            }
+            // System.out.println();
+            // System.out.println("Ma tran vua nhap voi " + n + " hang va " + m + " cot:");
+            // for (int i = 0 ; i < n ; i++)
+            // {
+            //     System.out.println();
+            //     for(int j = 0 ; j < m ; j++)
+            //     {
+            //         System.out.print(a[i][j] + " ");
+            //     }
+            // }
 
-            System.out.println();
+            // System.out.println();
 
-            scanner.close();
+            // scanner.close();
          }  
     }
